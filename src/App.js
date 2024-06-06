051022b6e9a2872dc0fbcbb63e1449d7c5117f08
DJTechnoo <askeltechno@hotmail.com>
2024-06-06 16:33:38 +0200

    Added basename.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 function App() {
 
     return (
-        <BrowserRouter>
+        <BrowserRouter basename='/casino-world'>
             <div className='flex'>
                 <Menu/>
                 <Arena/>
