95dfa2cad6464954153f0bfc94fe651fd2295c7e
DJTechnoo <askeltechno@hotmail.com>
2024-06-07 10:44:13 +0200

    MOVED hashrouter up.

--- src/App.js
+++ src/App.js
@@ -6,12 +6,11 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 function App() {
 
     return (
-        <HashRouter basename='/casino-world'>
             <div className='flex'>
                 <Menu/>
                 <Arena/>
             </div>
-        </HashRouter>
+
     );
 }
 
