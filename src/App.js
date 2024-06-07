208a9637eebb317e9f8e60379e011dc5b5468966
DJTechnoo <askeltechno@hotmail.com>
2024-06-07 10:52:02 +0200

    MOVED hashrouter down, removed basename.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 function App() {
 
     return (
+        <HashRouter>
             <div className='flex'>
                 <Menu/>
                 <Arena/>
+                
             </div>
-
+        </HashRouter>
     );
 }
 
