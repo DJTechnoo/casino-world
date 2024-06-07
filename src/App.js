57beb3ee103164878906553ad224768c2f6cdced
DJTechnoo <askeltechno@hotmail.com>
2024-06-07 10:32:26 +0200

    Replaced routing with HashRouter.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import Menu from "./components/Menu";
 import Arena from "./components/arena-components/Arena";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 
 function App() {
 
     return (
-        <BrowserRouter basename='/casino-world'>
+        <HashRouter basename='/casino-world'>
             <div className='flex'>
                 <Menu/>
                 <Arena/>
-                
             </div>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
